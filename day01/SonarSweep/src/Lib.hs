35ef2104ea74f79309c0ef221fca868d26202892
Daniel Stewart <mgdog1@googlemail.com>
2021-12-01 23:13:20 +0000

    initial function implementation

--- day01/SonarSweep/src/Lib.hs
+++ day01/SonarSweep/src/Lib.hs
@@ -1,7 +1,12 @@
 module Lib
   ( numDepthIncreases,
+    MeasurementWindow (..),
+    Depth (..),
+    measurementSums,
   )
 where
+
+import Data.Map (elems, empty, fromListWith, insertWith)
 
 numDepthIncreases :: Ord a => [a] -> Int
 numDepthIncreases = numDepthIncreases' 0
@@ -12,3 +17,12 @@ numDepthIncreases = numDepthIncreases' 0
       if x' > x
         then numDepthIncreases' (acc' + 1) (x' : xs')
         else numDepthIncreases' (acc') (x' : xs')
+
+data MeasurementWindow = A | B | C | D | E | F | G | H deriving (Ord, Eq)
+
+data Depth = Depth Int [MeasurementWindow]
+
+measurementSums :: [Depth] -> Int
+measurementSums depths = numDepthIncreases $ elems windowDepthTotals
+  where
+    windowDepthTotals = fromListWith (+) [(w, d) | (Depth d windows) <- depths, w <- windows]
--- day01/SonarSweep/test/Spec.hs
+++ day01/SonarSweep/test/Spec.hs
@@ -7,7 +7,43 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 main :: IO ()
-main = defaultMain numDepthIncreasesTests
+main =
+  defaultMain $
+    testGroup
+      "SonarSweep Tests"
+      [numDepthIncreasesTests, measurementSumsTests]
+
+measurementSumsTests =
+  testGroup
+    "measurementSumsTests"
+    [ testCase "No measurement sum for empty list" $
+        measurementSums [] @?= 0,
+      testCase "No measurement sum singleton list" $
+        measurementSums [d1]
+          @?= 0,
+      testCase
+        "Basic increase F -> G and Equality check on H"
+        $ measurementSums [d8, d9]
+          @?= 1,
+      testCase
+        "Basic decrease A -> B"
+        $ measurementSums [d1, d2]
+          @?= 0,
+      testCase
+        "Sample dataset"
+        $ measurementSums [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10] @?= 5
+    ]
+  where
+    d1 = Depth 199 [A]
+    d2 = Depth 200 [A, B]
+    d3 = Depth 208 [A, B, C]
+    d4 = Depth 210 [B, C, D]
+    d5 = Depth 200 [E, C, D]
+    d6 = Depth 207 [E, F, D]
+    d7 = Depth 240 [E, F, G]
+    d8 = Depth 269 [F, G, H]
+    d9 = Depth 260 [G, H]
+    d10 = Depth 263 [H]
 
 numDepthIncreasesTests =
   testGroup
