8311bc4cc0eb76c3f03006a4f5c559f8f9894a26
Daniel Stewart <mgdog1@googlemail.com>
2021-12-02 20:20:29 +0000

    finish dive part 1

--- day02/Dive/app/Main.hs
+++ day02/Dive/app/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import Lib
+import Control.Monad
+import Lib (Command, getPos)
+import System.IO
 
 main :: IO ()
-main = 
-    do
-        putStrLn "Hello"
+main = do
+  handle <- openFile "inputs.txt" ReadMode
+  contents <- hGetContents handle
+
+  let commands = readCommands $ lines contents
+  putStrLn $ "Commands Loaded: " ++ (show $ length $ commands)
+
+  let pos@(h, d) = getPos commands
+  putStrLn $ "Pos: " ++ (show pos)
+  putStrLn $ "Horizontal * Depth: " ++ show (h * d)
+  where
+    readCommands :: [String] -> [Command]
+    readCommands = map read
--- day02/Dive/src/Lib.hs
+++ day02/Dive/src/Lib.hs
@@ -4,12 +4,33 @@ module Lib
   )
 where
 
-data Command = Forward Int | Down Int | Up Int
+import Data.Char (digitToInt, toLower)
+import Data.List (intersperse)
+import Data.Maybe (fromJust, isNothing)
+import Numeric (readInt)
+import Text.Read (readMaybe)
+
+data Command = Forward Int | Down Int | Up Int deriving (Show)
+
+instance Read Command where
+  readsPrec _ input =
+    let (direction : magnitude : rest) = words input
+        d = case map toLower direction of
+          "forward" -> Just Forward
+          "down" -> Just Down
+          "up" -> Just Up
+          _ -> Nothing
+        m = readMaybe magnitude :: Maybe Int
+        validParse = not $ isNothing d || isNothing m
+        result
+          | isNothing d || isNothing m = Forward 0
+          | otherwise = fromJust d $ fromJust m
+     in [(result, unwords rest)]
 
 getPos :: [Command] -> (Int, Int)
-getPos = 
-    foldl accum (0, 0)
-    where
-        accum (hpos, depth) (Forward x) = (hpos + x, depth) 
-        accum (hpos, depth) (Up x) = (hpos, depth - x)
-        accum (hpos, depth) (Down x) = (hpos, depth + x)
+getPos =
+  foldl accum (0, 0)
+  where
+    accum (hpos, depth) (Forward x) = (hpos + x, depth)
+    accum (hpos, depth) (Up x) = (hpos, depth - x)
+    accum (hpos, depth) (Down x) = (hpos, depth + x)
