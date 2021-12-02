68abc335d1e2ac8c47386ee0625db6ee61b7c236
Daniel Stewart <mgdog1@googlemail.com>
2021-12-02 18:57:50 +0000

    tests passing

--- day02/Dive/src/Lib.hs
+++ day02/Dive/src/Lib.hs
@@ -7,4 +7,9 @@ where
 data Command = Forward Int | Down Int | Up Int
 
 getPos :: [Command] -> (Int, Int)
-getPos = undefined
+getPos = 
+    foldl accum (0, 0)
+    where
+        accum (hpos, depth) (Forward x) = (hpos + x, depth) 
+        accum (hpos, depth) (Up x) = (hpos, depth - x)
+        accum (hpos, depth) (Down x) = (hpos, depth + x)
