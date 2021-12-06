bedeaff55d50f3eb6da2ca36852e975c36810360
Daniel Stewart <mgdog1@googlemail.com>
2021-12-06 19:49:30 +0000

    tests passing

--- day03/BinaryDiagnostic/src/Lib.hs
+++ day03/BinaryDiagnostic/src/Lib.hs
@@ -1,14 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Lib
   ( getPowerConsumption,
     getGammaAndEpsilon,
     BinaryNum,
     stringToBn,
+    getGamma,
+    getEpsilon,
+    bnToInt,
+    transpose,
+    unwrap
   )
 where
 
 import Data.Char (digitToInt)
+import Data.List (foldl')
 
 newtype BinaryNum = BinaryNum [Int] deriving (Show)
+
+instance Eq BinaryNum where
+  (==) a b = bnToInt a == bnToInt b
+
 
 stringToBn :: String -> Maybe BinaryNum
 stringToBn s =
@@ -24,4 +36,43 @@ getPowerConsumption readings =
    in gamma * epsilon
 
 getGammaAndEpsilon :: [BinaryNum] -> (Int, Int)
-getGammaAndEpsilon = undefined
+getGammaAndEpsilon readings = (bnToInt gamma, bnToInt epsilon)
+  where
+    gamma = getGamma readings
+    epsilon = getEpsilon gamma
+
+getGamma :: [BinaryNum] -> BinaryNum
+getGamma nums = BinaryNum $ foldl accum [] $ transpose $ map unwrap nums
+  where
+    accum acc xs =
+        if zeros >= majorityThreshold' then
+            acc ++ [0]
+        else
+            acc ++ [1]
+        where
+            majorityThreshold' = if odd totalLen then  majorityThreshold + 1 else majorityThreshold
+            majorityThreshold = totalLen `div` 2
+            totalLen = length xs
+            zeros = length $ filter (==0) xs 
+
+getEpsilon :: BinaryNum -> BinaryNum
+getEpsilon (BinaryNum n) =
+  BinaryNum $
+    map
+      ( \case
+          1 -> 0
+          0 -> 1
+          _ -> error "Invalid binary num"
+      )
+      n
+
+bnToInt :: BinaryNum -> Int
+bnToInt (BinaryNum bn) = foldl (\acc x -> acc * 2 + x) 0 bn
+
+transpose :: [[a]] -> [[a]]
+transpose [] = []
+transpose ([] : _) = []
+transpose x = (map head x) : transpose (map tail x)
+
+unwrap :: BinaryNum -> [Int]
+unwrap (BinaryNum bn) = bn
--- day03/BinaryDiagnostic/test/Spec.hs
+++ day03/BinaryDiagnostic/test/Spec.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Maybe (fromJust)
 import Lib
+import Lib (bnToInt)
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -10,7 +11,82 @@ main =
   defaultMain $
     testGroup
       "BinaryDiagnostic Tests"
-      [getPowerConsumptionTests]
+      [bnToIntTests, getEpsilonTests, getGammaTests, getPowerConsumptionTests]
+
+bnToIntTests =
+  testGroup
+    "bnToIntTests"
+    [ testCase "Test 0" $ (bnToInt $ getBn "0") @?= 0,
+      testCase "Test 1" $ (bnToInt $ getBn "1") @?= 1,
+      testCase "Test 0101" $ bnToInt (getBn "0101") @?= 5
+    ]
+
+getGammaTests =
+  testGroup
+    "getGammaTests"
+    [ testCase "Test all empty" $
+        getGamma [] @?= getBn "",
+      testCase "Test 0" $
+        getGamma [getBn "0"] @?= getBn "0",
+      testCase "Test 1" $
+        getGamma [getBn "1"] @?= getBn "1",
+      testCase "Test multiple numbers (even)" $
+        getGamma
+          [ getBn "1",
+            getBn
+              "0"
+          ]
+          @?= getBn "0",
+      testCase "Test multiple numbers (odd)" $
+        getGamma
+          [ getBn "1",
+            getBn
+              "0",
+            getBn "1"
+          ]
+          @?= getBn "1",
+      testCase "Test bigger set of numbers" $
+        getGamma
+          [ getBn
+              "00100",
+            getBn
+              "11110",
+            getBn
+              "10110",
+            getBn
+              "10111",
+            getBn
+              "10101",
+            getBn
+              "01111",
+            getBn
+              "00111",
+            getBn
+              "11100",
+            getBn
+              "10000",
+            getBn
+              "11001",
+            getBn
+              "00010",
+            getBn
+              "01010"
+          ]
+          @?= getBn "10110"
+    ]
+
+getEpsilonTests =
+  testGroup
+    "getEpsilonTests"
+    [ testCase "Test all empty" $
+        getEpsilon (getBn "") @?= getBn "",
+      testCase "Test all 0" $
+        getEpsilon (getBn "0000") @?= getBn "1111",
+      testCase "Test all 1" $
+        getEpsilon (getBn "1111") @?= getBn "0000",
+      testCase "Test num 4 is 11" $
+        getEpsilon (getBn "0100") @?= getBn "1011"
+    ]
 
 getPowerConsumptionTests =
   testGroup
@@ -26,7 +102,8 @@ getPowerConsumptionTests =
       testCase
         "Test full"
         $ getPowerConsumption
-          [ getBn "11110",
+          [ getBn "00100",
+            getBn "11110",
             getBn
               "10110",
             getBn
@@ -50,5 +127,5 @@ getPowerConsumptionTests =
           ]
           @?= 198
     ]
-  where
-    getBn = fromJust . stringToBn
+
+getBn = fromJust . stringToBn
